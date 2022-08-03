57c491582ee2da847536389f734056a2f065d8e6
ksee1230 <ksee1230@gmail.com>
2022-08-03 22:32:48 +0900

    프로그래머스 - 쿼드압축 후 개수 세기(68936) - 수정

--- programmers/quadComp.py
+++ programmers/quadComp.py
@@ -4,7 +4,7 @@
 # 0이 4이고, 1이 0면 [1,0] 을 return, 반대의 경우 [0,1] 을 return, 둘 다 아니라면 각각 더한 값을 배열에 넣어 return 한다
 # 이렇게 다시 원래까지 돌아와서 최종적으로 다 더한 값을 return 한다
 
-def solution(arr):
+def solution1(arr):
     if len(arr) == 1:
         if arr[0][0] == 0:
             return [1,0]
@@ -30,3 +30,24 @@ def solution(arr):
         return answer
 
 # 이렇게 했는데, 문제가 풀어지긴 하나 시간초과가 난다.. 큰 것부터 그냥 계산을 하고 넘어가는 식으로 해야할 듯 하다
+# 배열 slicing으로는 한계가 있는 듯 함
+
+def solution(arr):
+    answer = [0, 0]
+    length = len(arr)
+    
+    def quadComp(a, b, l):
+        tg = arr[a][b]
+        for i in range (a, a+l):
+            for j in range (b, b+l):
+                if arr[i][j] != tg:
+                    l = l//2
+                    quadComp(a, b, l)
+                    quadComp(a+l, b, l)
+                    quadComp(a, b+l, l)
+                    quadComp(a+l, b+l, l)
+                    return
+        answer[tg]+=1
+    
+    quadComp(0,0,length)
+    return answer
